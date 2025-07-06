f1dca55cfb978ccaa414b090d177d1e6ad1f1e35
allichenabil <allichenabilpediatric@gmail.com>
2025-07-06 16:49:59 +0100

    initial

--- authMiddleware.js
+++ authMiddleware.js
@@ -1,25 +1,26 @@
-const jwt = require('jsonwebtoken');
-const jwtSecret = 'a_very_secure_and_unique_secret_key_for_your_medical_app_2025'; // Should be the same as in authRoutes.js
-
+const jwt = require("jsonwebtoken");
+const jwtSecret = process.env.JWT_SECRET;
 const authMiddleware = (req, res, next) => {
-    const authHeader = req.headers.authorization;
+  const authHeader = req.headers.authorization;
 
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        return res.status(401).json({ message: 'Access denied. No token provided.' });
-    }
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res
+      .status(401)
+      .json({ message: "Access denied. No token provided." });
+  }
 
-    const token = authHeader.split(' ')[1];
-    console.log('Received token:', token);
+  const token = authHeader.split(" ")[1];
+  console.log("Received token:", token);
 
-    try {
-        const decoded = jwt.verify(token, jwtSecret);
-        req.user = decoded; // Add user payload to the request
-        console.log('Token decoded successfully:', decoded);
-        next();
-    } catch (ex) {
-        console.error('Token verification failed:', ex.message);
-        res.status(400).json({ message: 'Invalid token.' });
-    }
+  try {
+    const decoded = jwt.verify(token, jwtSecret);
+    req.user = decoded; // Add user payload to the request
+    console.log("Token decoded successfully:", decoded);
+    next();
+  } catch (ex) {
+    console.error("Token verification failed:", ex.message);
+    res.status(400).json({ message: "Invalid token." });
+  }
 };
 
 module.exports = authMiddleware;
